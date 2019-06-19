0bd0e1ec1806b8438ea209263eaffce8bd7f427e
Lucas Albuquerque Pires <50931185+LucasPires50@users.noreply.github.com>
2019-06-19 09:26:55 -0300

    Calculo da média

---
+++ média.java
@@ -0,0 +1,17 @@
+package logprogramaçao;
+
+public class LogProgramaçao {
+
+    public static void main(String[] args)
+    
+        int nota1, nota2, nota3;
+        
+        nota1 = 7; 
+        nota2 = 9; 
+        nota3 = 10;
+        int resultado;
+        
+        resultado = (nota1 + nota2 + nota3) / 3;
+        System.out.println("O resultado é "+resultado);   
+    }   
+}
